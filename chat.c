3c91fdec548964fb19d76ffcb9eb641b7b134d26
huanglei104 <huanglei104@gmail.com>
2021-01-21 16:18:15 +0800

    use socket only one

--- chat.c
+++ chat.c
@@ -6,121 +6,124 @@
 #include <errno.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-int recvfd, sendfd;
+int sockfd;
 char *username;
 
-void* recv_process(void *args)
-{
-	int optval, ret;
-	char msg[1024];
-	socklen_t len;
-	struct sockaddr_in recvAddr;
-
-	memset(msg, 0, 1024);
-
-	recvfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (recvfd == -1) {
-		perror("socket");
-		return NULL;
-	}
-
-	optval = 1;
-	len = sizeof(recvAddr);
-
-	ret = setsockopt(recvfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
-	if (ret == -1) {
-		perror("setsockopt");
-		close(recvfd);
-		return NULL;
-	}
-
-	memset(&recvAddr, 0, sizeof(recvAddr));
-	recvAddr.sin_family = AF_INET;
-	recvAddr.sin_port = htons(4001);
-	recvAddr.sin_addr.s_addr = INADDR_ANY;
-
-	// 必须绑定，否则无法监听
-	ret = bind(recvfd, (struct sockaddr*)&recvAddr, len);
-	if (ret == -1) {
-		perror("bind");
-		close(recvfd);
-		return NULL;
-	}
-
-	while (1) {
-		//ret = recvfrom(recvfd, msg, 1024, 0, (struct sockaddr*)&recvAddr, &len);
-		ret = recv(recvfd, msg, 1024, 0);
-		if (ret == -1) {
-			perror("recvfrom");
-			continue;
-		}
-
-		printf("%s\n", msg);
-	}
-
-	close(recvfd);
-
-	return NULL;
-}
-
-int send_msg()
+int start_send_msg()
 {
 	int ret;
-	struct sockaddr_in bcaddr;
 	char msg[1024];
+	struct sockaddr_in bcaddr;
 
-	memset(&bcaddr, 0, sizeof(struct sockaddr_in));
-	memset(&msg, 0, 1024);
+	memset(&bcaddr, 0, sizeof(bcaddr));
 	bcaddr.sin_family = AF_INET;
 	inet_aton("255.255.255.255", &bcaddr.sin_addr);
-	bcaddr.sin_port = htons(4001);
-
-	sprintf(msg, "[%s]", username);
+	bcaddr.sin_port = htons(4567);
 
 	while (1) {
+		memset(&msg, 0, 1024);
+		sprintf(msg, "[%s]", username);
+
 		scanf("%s", msg + strlen(username) + 2);
 
-		ret = sendto(sendfd, msg, strlen(msg), 0, (struct sockaddr*)&bcaddr, sizeof(bcaddr));
+		ret = sendto(sockfd, msg, strlen(msg), 0,
+				(struct sockaddr*)&bcaddr,
+				sizeof(bcaddr));
 		if (ret == -1) {
 			perror("sendto");
 			continue;
 		}
 	}
 
-	close(sendfd);
+	close(sockfd);
+}
+
+int init(int argc, char *argv[])
+{
+	int ret;
+	int optval;
+	struct sockaddr_in local_addr;
+
+	if (argc != 2) {
+		printf("no username\n");
+		return EXIT_FAILURE;
+	}
+
+	username = argv[1];
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd == -1) {
+		perror("socket");
+		return EXIT_FAILURE;
+	}
+
+	optval = 1;
+
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+			&optval,
+			sizeof(optval));
+	if (ret == -1) {
+		perror("setsockopt SO_REUSEADDR");
+		close(sockfd);
+		return EXIT_FAILURE;
+	}
+
+	memset(&local_addr, 0, sizeof(local_addr));
+	local_addr.sin_family = AF_INET;
+	local_addr.sin_addr.s_addr = htons(INADDR_ANY);
+	local_addr.sin_port = htons(4567);
+
+	ret = bind(sockfd, (struct sockaddr*)&local_addr,
+			sizeof(local_addr));
+	if (ret == -1) {
+		perror("bind");
+		close(sockfd);
+		return EXIT_FAILURE;
+	}
+
+	ret = setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST,
+			&optval,
+			sizeof(optval));
+	if (ret == -1) {
+		perror("setsockopt SO_BROADCAST");
+		close(sockfd);
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+void* recv_msg(void *args)
+{
+	char buf[1024];
+
+	while (1) {
+		memset(buf, 0, 1024);
+		recvfrom(sockfd, buf, 1024, 0, NULL, NULL);
+		printf("%s\n", buf);
+	}
+
+	return NULL;
+}
+
+void start_recv_msg()
+{
+	pthread_t recv_thread;
+
+	pthread_create(&recv_thread, NULL, recv_msg, NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	int ret;
-	int optval;
-	pthread_t recv_thread;
-
-	if (argc != 2) {
-		printf("error: no username\n");
-		return 0;
+	if (init(argc, argv) == EXIT_FAILURE) {
+		return EXIT_FAILURE;
 	}
 
-	optval = 1;
-	username = argv[1];
+	start_recv_msg();
 
-	sendfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sendfd == -1) {
-		perror("socket");
-		return 0;
-	}
-
-	ret = setsockopt(sendfd, SOL_SOCKET, SO_BROADCAST | SO_REUSEADDR, &optval, sizeof(int));
-	if (ret == -1) {
-		perror("setsockopt");
-		close(sendfd);
-		return 0;
-	}
-
-	pthread_create(&recv_thread, NULL, recv_process, NULL);
-
-	send_msg();
+	start_send_msg();
 
 	return 0;
 }
